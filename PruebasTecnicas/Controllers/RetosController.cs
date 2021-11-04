1f36bc1c88efb9cba50b9bc698a2bce5894a8573
J3c4 <jeca1828s@gmail.com>
2021-11-03 18:31:59 -0600

    ok
    ok

---
+++ PruebasTecnicas/Controllers/RetosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebasTecnicas.Controllers
+{
+    public class RetosController : Controller
+    {
+        // GET: Retos
+        public ActionResult Sencillos()
+        {
+            return View();
+        }
+    }
+}
---
+++ PruebasTecnicas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PruebasTecnicas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
